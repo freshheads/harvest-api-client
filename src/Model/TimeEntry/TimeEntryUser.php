b7e0b79ad60e04a2739519fd2dd416a70299252b
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2019-01-07 10:10:23 +0100

    Make models more open, to allow for harvest API adjustments

--- src/Model/TimeEntry/TimeEntryUser.php
+++ src/Model/TimeEntry/TimeEntryUser.php
@@ -16,19 +16,23 @@ final class TimeEntryUser
     private $id;
     private $name;
 
-    public function __construct(int $id, string $name)
-    {
-        $this->id = $id;
-        $this->name = $name;
-    }
-
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): string
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): ?string
     {
         return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
     }
 }
